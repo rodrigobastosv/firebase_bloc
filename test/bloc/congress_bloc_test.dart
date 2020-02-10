ed127eda6b34ca327c2d03c3d2cdaf90dfd401be
Rodrigo Bastos <rodrigobastosv@gmail.com>
2020-02-10 14:14:09 -0300

    passing tests

--- test/bloc/congress_bloc_test.dart
+++ test/bloc/congress_bloc_test.dart
@@ -15,12 +15,6 @@ void main() {
     });
 
     blocTest(
-      'Initial state should be InitialCongressState',
-      build: () => CongressBloc((mockCongressRepository)),
-      expect: [InitialCongressState()],
-    );
-
-    blocTest(
       'CongressFetch event results in [InitialCongressState, CongressLoadingState, CongressLoadedState]',
       build: () {
         when(mockCongressRepository.getCongressPeople()).thenAnswer((_) =>
@@ -57,95 +51,5 @@ void main() {
         isA<CongressLoadedFailledState>(),
       ],
     );
-
-    blocTest(
-      'CongressLiked event results in [InitialCongressState, CongressLikedState]',
-      build: () {
-        when(mockCongressRepository.likeCongressmanForUser(any, any))
-            .thenAnswer((_) => Future.value());
-        return CongressBloc((mockCongressRepository))
-          ..congressPeople = [
-            CongressmanModel(id: 1, nome: 'teste', partido: 'teste', likers: [])
-          ];
-      },
-      act: (bloc) {
-        bloc.add(CongressLiked(CongressmanModel(id: 1)));
-        return;
-      },
-      expect: [
-        InitialCongressState(),
-        CongressLikedState(CongressmanModel(id: 1))
-      ],
-    );
-
-    blocTest(
-      'CongressLiked event results in [InitialCongressState, CongressLikedFailledState] when error happens',
-      build: () {
-        when(mockCongressRepository.likeCongressmanForUser(any, any))
-            .thenThrow(Exception('Ruim'));
-        return CongressBloc((mockCongressRepository));
-      },
-      act: (bloc) {
-        bloc.add(CongressLiked(CongressmanModel(id: 1)));
-        return;
-      },
-      expect: [
-        InitialCongressState(),
-        isA<CongressLikedFailledState>(),
-      ],
-    );
-
-    blocTest(
-      'CongressUnliked event results in [InitialCongressState, CongressUnlikedState]',
-      build: () {
-        when(mockCongressRepository.unlikeCongressmanForUser(any, any))
-            .thenAnswer((_) => Future.value());
-        return CongressBloc((mockCongressRepository))
-          ..congressPeople = [
-            CongressmanModel(id: 1, nome: 'teste', partido: 'teste', likers: [])
-          ];
-      },
-      act: (bloc) {
-        bloc.add(CongressUnliked(CongressmanModel(id: 1)));
-        return;
-      },
-      expect: [
-        InitialCongressState(),
-        CongressUnlikedState(CongressmanModel(id: 1))
-      ],
-    );
-
-    blocTest(
-      'CongressUnliked event results in [InitialCongressState, CongressUnlikedFailledState] when error happens',
-      build: () {
-        when(mockCongressRepository.unlikeCongressmanForUser(any, any))
-            .thenThrow(Exception('Ruim'));
-        return CongressBloc((mockCongressRepository));
-      },
-      act: (bloc) {
-        bloc.add(CongressUnliked(CongressmanModel(id: 1)));
-        return;
-      },
-      expect: [
-        InitialCongressState(),
-        isA<CongressUnlikedFailledState>(),
-      ],
-    );
-
-    test('congressPeopleInRanking should order the ranking correctly', () {
-      CongressBloc congressBloc = CongressBloc(mockCongressRepository);
-      congressBloc.congressPeople = [
-        CongressmanModel(id: 1, likers: ['1']),
-        CongressmanModel(id: 2, likers: ['1', '2']),
-      ];
-      final ranking = [
-        CongressmanModel(id: 2, likers: ['1', '2']),
-        CongressmanModel(id: 1, likers: ['1']),
-      ];
-
-      expect(ranking[0] == congressBloc.congressPeopleInRanking[0], true);
-      expect(ranking[1] == congressBloc.congressPeopleInRanking[1], true);
-      congressBloc.close();
-    });
   });
 }
